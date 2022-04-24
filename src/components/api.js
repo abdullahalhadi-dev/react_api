f628ccbd3ab774b31db24749125ee87cf3b69600
Abdullah Al Hadi <abdullahalhadi317@gmail.com>
2022-04-24 19:28:21 +0600

    update

--- src/components/api.js
+++ src/components/api.js
@@ -1,35 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import './Api.css';
+import React, { useEffect, useState } from "react";
+import "./Api.css";
 
 const API = () => {
+  const [users, setUsers] = useState([]);
 
-    const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
 
-    useEffect(() => {
-        fetch('https://restcountries.com/v3.1/all')
-        .then(res => res.json())
-        .then(data => setUsers(data));
-    }, []);
-
-    return (
-        <div>
-            <div className='api'>
-                {
-                    users.map(collectData => {
-                        return(
-                            <div>
-                                <ul>
-                                    <li className='style1'>Country: {collectData.name.common}</li>
-                                    <li className='style2'>Country Flag: <img {...collectData.flags.png} alt="Country Flag" /> </li>
-                                    <li className='style3'>Language: </li>
-                                </ul>
-                            </div>
-                        )
-                    })
-                }
+  return (
+    <div>
+      <div className="api">
+        {users.map((collectData) => {
+          return (
+            <div>
+              <ul>
+                <li className="style1">Country: {collectData.name.common}</li>
+                <li className="style2">
+                  Country Flag:{" "}
+                  <img src={collectData.flags.png} alt="Country Flag" />{" "}
+                </li>
+                <li className="style3">
+                  Language:{" "}
+                  {collectData.languages &&
+                    Object.values(collectData.languages).map((lan) => (
+                      <>
+                        <span>{lan}</span>
+                      </>
+                    ))}
+                </li>
+              </ul>
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default API;
